9e897b621be95931d804c7d6042357677cd151ef
alisson-campos <glauberalisson@gmail.com>
2023-03-29 04:34:22 -0300

    Ajustes console.log

--- app/infrastructure/config/database.js
+++ app/infrastructure/config/database.js
@@ -2,7 +2,6 @@ const {connect} = require("mongoose");
 
 async function connectDB(){
     try {
-        console.log(process.env.MONGO_URI)
         await connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 const  dotenv = require("dotenv");
 const  express = require("express");
-const  shortid = require("shortid");
-const  Url = require("./app/infrastructure/database/schemas/Url");
-const  utils = require("./app/Util/util");
 const { connectDB } = require("./app/infrastructure/config/database")
 const router = require("./app/ports/http/routes");
-const responseTimeLogger = require("./app/ports/http/middlewares/responseTimeLogger")
-
 
 dotenv.config();
 connectDB();
